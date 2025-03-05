da3e131feaf713a223f7f4fc22389955ff267ffe
Naufal Laundra <140129728+PacReborn1511@users.noreply.github.com>
2025-03-05 21:59:31 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Toggle class active
+const navSlide = () =>{
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector(".nav");
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+        
+    };
+
+    navSlide();
+
